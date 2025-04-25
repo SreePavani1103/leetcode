02c9f199643960eeacc7370757809ce451cb00bf
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2025-04-25 21:23:15 +0530

    Time: 0 ms (100%), Space: 8.5 MB (14.72%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.c
@@ -0,0 +1,14 @@
+int arraySign(int* nums, int numsSize) {
+    int n=numsSize;
+    int long long p=1;
+    for(int i=0;i<n;i++)
+    {
+       if(nums[i]==0)
+       return 0;
+       else if(nums[i]<0)
+       {
+        p*=-1;
+       }
+    }
+    return p;
+}
