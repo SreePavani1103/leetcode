5074cc6ece664c0d77780ad705c528372bc683c0
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2025-04-26 21:34:11 +0530

    Time: 4 ms (28.57%), Space: 8.6 MB (48.57%) - LeetHub

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.c
@@ -0,0 +1,56 @@
+void merge_sort(int a[],int l,int m,int h)
+{
+    int res[h-l+1];
+    int k=0;
+    int i=l;
+    int j=m+1;
+    while(i<=m && j<=h)
+    {
+        if(a[i]<=a[j])
+        {
+            res[k++]=a[i++];
+        }
+        else
+        res[k++]=a[j++];
+    }
+    while(i<=m)
+    {
+        res[k++]=a[i++];
+    }
+    while(j<=h)
+    {
+        res[k++]=a[j++];
+    }
+     k=0;
+    for(int i=l;i<=h;i++)
+    {
+      a[i]=res[k++];
+    }
+}
+void merge(int a[],int l,int h)
+{
+    if(l<h)
+    {
+        int m=(l+h)/2;
+        merge(a,l,m);
+        merge(a,m+1,h);
+        merge_sort(a,l,m,h);
+    }
+}
+int minMovesToSeat(int* seats, int seatsSize, int* students, int studentsSize) {
+  int n=seatsSize;
+  int m=studentsSize;
+  merge(seats,0,n-1);
+  merge(students,0,m-1);
+  int b[n];
+  for(int i=0;i<n;i++)
+  {
+    b[i]=abs(seats[i]-students[i]);
+  }
+  int sum=0;
+  for(int i=0;i<n;i++)
+  {
+      sum+=b[i];
+  }
+    return sum;
+}
