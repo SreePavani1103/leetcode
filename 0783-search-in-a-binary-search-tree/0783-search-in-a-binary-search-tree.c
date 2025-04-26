ae5bfb118bf0333f7a672968cade4c364675763e
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2025-04-26 20:12:51 +0530

    Time: 0 ms (100%), Space: 18.8 MB (17.08%) - LeetHub

---
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if(root==NULL||root->val==val)
+    return root;
+    else if(root->val>val)
+    return searchBST(root->left,val);
+    else if(root->val<val)
+    return searchBST(root->right,val);
+    else
+    return root;
+}
