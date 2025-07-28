5fe827d4ecc9b6676b8a6aac8986960322c16c56
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2025-07-28 21:04:24 +0530

    Time: 95 ms (68.25%), Space: 49.2 MB (70.62%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<2)
+        return 0;
+        boolean[] isprime = new boolean[n];
+        Arrays.fill(isprime,true);
+        isprime[0]=false;
+        isprime[1]=false;
+        for(int i=2;i*i<n;i++)
+        {
+            if(isprime[i])
+            {
+                for(int j=i*i;j<n;j+=i)
+                isprime[j]=false;
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(isprime[i])
+            c++;
+        }
+        return c;
+   }
+}
