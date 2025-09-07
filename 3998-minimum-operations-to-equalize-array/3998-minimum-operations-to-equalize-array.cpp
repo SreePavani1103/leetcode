4f9b44861aee0140f91610493aa555194a2543da
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2025-09-07 22:01:13 +0530

    Time: 0 ms (100%), Space: 30.7 MB (91.67%) - LeetHub

---
+++ 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
+        return 0;
+        for(int i=1;i<n;i++)
+            {
+                if(nums[i]!=nums[i-1])
+                return 1;
+            }
+            return 0;
+       
+    }
+};
