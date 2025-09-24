01ec4b7441e124250ccb142b33e7e5e0666d41e8
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2025-09-24 15:03:34 +0530

    Time: 135 ms (5.25%), Space: 80 MB (5.7%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n=nums.size();
-        int m=nums[0];
-        int m2=nums[0];
-        for(int i=0;i<n;i++)
-        {
-            m=max(m,nums[i]);
-           m2=min(m2,nums[i]);
-        }
+        int n=nums.size(); 
         map<int,int>mpp;
         for(int c:nums)
         {
