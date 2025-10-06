0050d150ba78312ef6169f014f665db493013b22
Yamuna Sri Pavani Uriti <uritiyamunasripavani@gmail.com>
2025-10-06 12:00:30 +0530

    Time: 773 ms (5.23%), Space: 477.1 MB (5.23%) - LeetHub

--- 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.cpp
+++ 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    
+    vector<int> fun(vector<int>&a)
+    {
+        vector<int>v;
+        int m=1e9+7;
+               long long sum=0;
+        for(int i=0;i<a.size();i++)
+        {
+              sum=(sum+a[i])%m;
+              v.push_back(sum);
+        }
+        return v;
+    }
     int valueAfterKSeconds(int n, int k) {
        vector<int>v(n,1);
        int mod=1e9+7;
-       for(int i=1;i<=k;i++)
-       {
-           for(int j=1;j<n;j++)
-           {
-               v[j]=(v[j-1]%mod+v[j]%mod)%mod;
-           }
-       }
-       return v[n-1]%mod;
+        for(int i=0;i<k;i++)
+        {
+            v=fun(v);
+        }
+        return v[v.size()-1]%mod;
     }
 };
